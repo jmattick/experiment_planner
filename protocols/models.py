29f567868c3c7e31048d4b97165131a9ec34b8db
jmatty16 <jmatty1661@gmail.com>
2020-01-05 13:16:24 -0600

    add protocol and step models

--- protocols/models.py
+++ protocols/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Protocol(models.Model):
+    name = models.CharField(max_length=200)
+    days = models.IntegerField(default=1)
+    description = models.CharField(max_length=200)
+
+class Step(models.Model):
+    protocol = models.ForeignKey(Protocol, on_delete=models.CASCADE)
+    step_number = models.IntegerField(default=0)
+    step_text = models.CharField(max_length=1000)
+    time_min = models.IntegerField(default=0)
+    flexible = models.BooleanField(default=False)

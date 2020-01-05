8dba011786da9e2d20c911fdffb1c2e5307ff398
jmatty16 <jmatty1661@gmail.com>
2020-01-05 12:57:39 -0600

    update url path and css

--- experiment_planner/urls.py
+++ experiment_planner/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='protocols/')),
     path('protocols/', include('protocols.urls')),
     path('admin/', admin.site.urls),
 ]

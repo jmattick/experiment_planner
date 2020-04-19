964eb4a5b455a8ec55d4418a32b01e60a2857411
jmattick <jmatty1661@gmail.com>
2020-04-19 01:49:13 -0500

    update style protocol form

--- experiment_planner/settings.py
+++ experiment_planner/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 ROOT_URLCONF = 'experiment_planner.urls'
 

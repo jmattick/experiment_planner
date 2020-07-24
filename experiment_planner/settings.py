53a5747bef150780c94c21f46bb06a923b52ab2c
jmattick <jmatty1661@gmail.com>
2020-07-23 22:07:29 -0500

    prepare for heroku

--- experiment_planner/settings.py
+++ experiment_planner/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +27,7 @@ SECRET_KEY = os.getenv("EXPERIMENT_PLANNER_SECRET_KEY")
 
 DEBUG = (os.environ.get('DJANGO_DEBUG') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://experiment-planner.herokuapp.com/']
 
 ACCOUNT_EMAIL_REQUIRED = True
 
@@ -162,6 +163,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'protocols:index'
@@ -179,3 +181,5 @@ SENDGRID_ECHO_TO_STDOUT=(os.environ.get('DJANGO_DEBUG') == 'True')
 SETTINGS_EXPORT = [
     'GOOGLE_CLIENT_ID',
 ]
+
+django_heroku.settings(locals())

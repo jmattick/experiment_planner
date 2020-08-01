4135e5db3a674aa247ffe78180083945b71454af
jmattick <jmatty1661@gmail.com>
2020-08-01 14:41:13 -0500

    update travis

--- experiment_planner/settings.py
+++ experiment_planner/settings.py
@@ -103,6 +103,18 @@ DATABASES = {
     }
 }
 
+# Database setup for Travis CI
+if 'TRAVIS' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE':   'django.db.backends.postgresql_psycopg2',
+            'NAME':     'travisci',
+            'USER':     'postgres',
+            'PASSWORD': '',
+            'HOST':     'localhost',
+            'PORT':     '',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
